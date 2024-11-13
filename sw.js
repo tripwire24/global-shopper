a9c7be69b6bcb1ac37031d9eef0ef5c302355c1d
tripwire24 <158844865+tripwire24@users.noreply.github.com>
2024-11-14 09:53:04 +1300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,12 +2,12 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('global-shopper-cache').then(cache => {
       return cache.addAll([
-        '/',
-        '/index.html',
-        '/app.js',
-        '/style.css',
-        '/icon-192.png',
-        '/icon-512.png'
+        './',
+        './index.html',
+        './app.js',
+        './style.css',
+        './icon-192.png',
+        './icon-512.png'
       ]);
     })
   );
@@ -16,7 +16,12 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+      return response || fetch(event.request).then(fetchResponse => {
+        return caches.open('global-shopper-cache').then(cache => {
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        });
+      });
     })
   );
 });
