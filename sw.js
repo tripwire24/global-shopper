2f643ce1134a5f7eeb099cd91e7114831d92a197
tripwire24 <158844865+tripwire24@users.noreply.github.com>
2024-11-13 13:47:30 +1300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,78 @@
+const CACHE_NAME = 'shopperex-v1';
+const STATIC_CACHE = 'static-cache-v1';
+const DYNAMIC_CACHE = 'dynamic-cache-v1';
+
+const STATIC_ASSETS = [
+    './',
+    './index.html',
+    './app.js',
+    './manifest.json',
+    './icon-192.png',
+    './icon-512.png',
+    'https://unpkg.com/react@17/umd/react.development.js',
+    'https://unpkg.com/react-dom@17/umd/react-dom.development.js',
+    'https://unpkg.com/@babel/standalone/babel.min.js',
+    'https://cdn.tailwindcss.com'
+];
+
+// Install Event
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(STATIC_CACHE)
+            .then(cache => cache.addAll(STATIC_ASSETS))
+            .then(() => self.skipWaiting())
+    );
+});
+
+// Activate Event
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        Promise.all([
+            caches.keys().then(keys => Promise.all(
+                keys.filter(key => key !== STATIC_CACHE && key !== DYNAMIC_CACHE)
+                    .map(key => caches.delete(key))
+            )),
+            self.clients.claim()
+        ])
+    );
+});
+
+// Fetch Event
+self.addEventListener('fetch', (event) => {
+    // Handle API requests
+    if (event.request.url.includes('api.exchangerate-api.com')) {
+        event.respondWith(
+            fetch(event.request)
+                .then(response => {
+                    const clonedResponse = response.clone();
+                    caches.open(DYNAMIC_CACHE)
+                        .then(cache => cache.put(event.request, clonedResponse));
+                    return response;
+                })
+                .catch(() => caches.match(event.request))
+        );
+        return;
+    }
+
+    // Handle static assets
+    event.respondWith(
+        caches.match(event.request)
+            .then(cachedResponse => {
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+                return fetch(event.request)
+                    .then(response => {
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+                        const responseToCache = response.clone();
+                        caches.open(DYNAMIC_CACHE)
+                            .then(cache => {
+                                cache.put(event.request, responseToCache);
+                            });
+                        return response;
+                    });
+            })
+    );
+});
