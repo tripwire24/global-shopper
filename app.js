d696fe1db26b45ee745b32e4b84628299bec9195
tripwire24 <158844865+tripwire24@users.noreply.github.com>
2024-11-14 09:48:47 +1300

    Update app.js

--- app.js
+++ app.js
@@ -156,4 +156,29 @@ async function registerServiceWorker() {
   }
 }
 
+// Service worker file: service-worker.js
+// Caching the app shell and assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('global-shopper-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/app.js',
+        '/style.css',
+        '/icon-192.png',
+        '/icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
 init();
