a575cbe192ba2e41be28d00b2623a4c22ef7cfff
tripwire24 <158844865+tripwire24@users.noreply.github.com>
2024-11-14 14:35:50 +1300

    Update app.js

--- app.js
+++ app.js
@@ -356,7 +356,7 @@ return (
                             </div>
                         </div>
 
-                                    <button
+                        <button
                             onClick={saveAndReset}
                             disabled={!fromAmount || !toAmount}
                             className={`w-full px-4 py-3 bg-indigo-600 text-white rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition disabled:opacity-50 disabled:cursor-not-allowed`}
